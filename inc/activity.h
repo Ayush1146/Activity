89b8c229403a8daf3a41efe1ebb6884eef57db63
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-28 00:34:48 +0530

    Update activity.h

--- inc/activity.h
+++ inc/activity.h
@@ -23,5 +23,18 @@
 #define LED_OFF_TIME    (2000)   /**< LED OFF time in milli seconds */
 
 
+/**
+ * Macro Definitions
+ */
+
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PB0)   /**< LED Pin number  */
+#define SW_PORT (PORTD) /**< SWITCH Port number */
+#define SW_PIN_0 (PD0)  /**< SWITCH 0 Pin number */
+#define SW_PIN_1 (PD1)  /**< SWITCH 1 Pin number */
+
+
 
 #endif /** __ACTIVITY_H_ */
