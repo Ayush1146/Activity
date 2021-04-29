f97cd418dec62d137952036e20c09049db462d39
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-29 15:07:39 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+
+#include "activity3.h"
+
+void setuppwm(){
+    TCCR1A |= (1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
+    TCCR1B |= (1<<WGM12)|(1<<CS11)|(1<<CS10); // 64 PRESCALAR
+
+    DDRB |= (1<<PB1);
+}
+
+
+void waveform(uint16_t temp)
+{
+    if(temp>=0 && temp<=200){
+        OCR1A=0xcc;
+
+    }
+    else if(temp>=210 && temp<=500){
+        OCR1A=0x199;
+
+    }
+    else if(temp>=510 && temp<=700){
+        OCR1A=0x2cc;
+
+    }
+    else if(temp>=710 && temp<=1024){
+        OCR1A=0x3cb;
+
+    }
+    else{
+        OCR1A = 0x3ff;  /*duty cycle is 100%*/
+
+    }
+
+}
+
