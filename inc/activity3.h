433a46f63c648265f6e1b1cf273caccfe5c9de9b
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-29 15:03:52 +0530

    Add files via upload

---
+++ inc/activity3.h
@@ -0,0 +1,15 @@
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void setuppwm();
+
+void waveform(uint16_t temp);
+
+
+
+#endif // ACTIVITY3_H_INCLUDED
