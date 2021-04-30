d90761549e1633a1d47ea1e6f88d1bb0f55cf7e6
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-30 13:42:59 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -2,8 +2,8 @@
 
 void USARTint(uint16_t ubrr_value){
 
- UBBRR0L = ubrr_value;
- UBBRR0H = (ubrr_value>>8)&0x00ff;
+ UBRR0L = ubrr_value;
+ UBRR0H = (ubrr_value>>8)&0x00ff;
  UCSR0A |= (1<<RXC0) | (1<<TXC0);//Receive and Transmit Complete
  UCSR0B |= (1<<RXCIE0)| (1<<TXCIE0)| (1<< RXEN0) |(1<< TXEN0);// Enable Tx ,Rx and Interrupt
  UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00); //8 bit size character size and by default asynchronous USART
