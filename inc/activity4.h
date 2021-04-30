55b5f625be58b7639f724fd2afebd3f778957400
Ayush Sharma <sharmayush009@gmail.com>
2021-04-30 12:39:07 +0530

    activity4

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,6 +1,14 @@
 #ifndef ACTIVITY4_H_INCLUDED
 #define ACTIVITY4_H_INCLUDED
 
+#define F_CPU 16000000UL
+
+void USARTint(uint16_t ubrr_value );
+
+void UARTwritechar(char data);
+
+char UARTreadchar();
+
 
 
 #endif // ACTIVITY4_H_INCLUDED
--- src/activity4.c
+++ src/activity4.c
@@ -1,1 +1,35 @@
 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "activity4.h"
+
+void USARTint(uint16_t ubrr_value){
+
+ UBBRR0L = ubrr_value;
+ UBBRR0H = (ubrr_value>>8)&0x00ff;
+ UCSR0A |= (1<<RXC0) | (1<<TXC0);//Receive and Transmit Complete
+ UCSR0B |= (1<<RXCIE0)| (1<<TXCIE0)| (1<< RXEN0) |(1<< TXEN0);// Enable Tx ,Rx and Interrupt
+ UCSR0C |= (1<<UCSZ01)|(1<<UCSZ00; //8 bit size character size and by default asynchronous USART
+
+}
+
+void UARTwritechar(char data){
+
+while(!(UCSR0A)& (1<<UDRE0))
+{
+
+}
+
+UDR0 = data;
+}
+
+char UARTreadchar(){
+while(!(UCSR0A)&(1<<RXC0))
+{
+
+}
+return UDR0;
+}
+
+
