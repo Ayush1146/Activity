bbba528d605d47c7905c9648f6f30bab2ac319a3
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-30 14:55:59 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,5 +1,6 @@
 
 #include "activity3.h"
+#include "activity4.h"
 
 void setuppwm(){
     TCCR1A |= (1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
@@ -12,23 +13,23 @@ void setuppwm(){
 void waveform(uint16_t temp)
 {
     if(temp>=0 && temp<=200){
-        OCR1A=0xcc;
+        OCR1A=0xcc;  //Output PWM 20%
 
     }
     else if(temp>=210 && temp<=500){
-        OCR1A=0x199;
+        OCR1A=0x199;//Output PWM 40%
 
     }
     else if(temp>=510 && temp<=700){
-        OCR1A=0x2cc;
+        OCR1A=0x2cc;//Output PWM 70%
 
     }
     else if(temp>=710 && temp<=1024){
-        OCR1A=0x3cb;
+        OCR1A=0x3cb;//Output PWM 95%
 
     }
     else{
-        OCR1A = 0x3ff; 
+        OCR1A = 0x3ff; //Output PWM 100%
 
     }
 
