a4a447d086f22c84faf647d63bfe97bc0d951a07
Ayush Sharma <65450162+Ayush1146@users.noreply.github.com>
2021-04-30 15:31:57 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -14,80 +14,80 @@ void setuppwm(){
 void waveform(uint16_t temp)
 {
     int i;
-    char temp[10];
+    char temprature[10];
     if(temp>=0 && temp<=200){
         OCR1A=0xcc;  //Output PWM 20%
-        temp[0] = '2';
-        temp[1] = '0';
-        temp[2] = 176;
-        temp[3] = 'C';
-        temp[4] = ' ';
-        temp[5] = '\0';
+        temprature[0] = '2';
+        temprature[1] = '0';
+        temprature[2] = 176;
+        temprature[3] = 'C';
+        temprature[4] = ' ';
+        temprature[5] = '\0';
 
         for(i=0;i<=5;i++){
 
-            UARTwritechar(temp[i]);
+            UARTwritechar(temprature[i]);
 
         }
         _delay_ms(20);
     }
     else if(temp>=210 && temp<=500){
         OCR1A=0x199;//Output PWM 40%
-        temp[0] = '2';
-        temp[1] = '5';
-        temp[2] = 176;
-        temp[3] = 'C';
-        temp[4] = ' ';
-        temp[5] = '\0';
+        temprature[0] = '2';
+        temprature[1] = '5';
+        temprature[2] = 176;
+        temprature[3] = 'C';
+        temprature[4] = ' ';
+        temprature[5] = '\0';
 
         for(i=0;i<=5;i++){
 
-          UARTwritechar(temp[i]);
+          UARTwritechar(temprature[i]);
         }
         _delay_ms(20);
     }
     else if(temp>=510 && temp<=700){
         OCR1A=0x2cc;//Output PWM 70%
-        temp[0] = '2';
-        temp[1] = '9';
-        temp[2] = 176;
-        temp[3] = 'C';
-        temp[4] = ' ';
-        temp[5] = '\0';
+        temprature[0] = '2';
+        temprature[1] = '9';
+        temprature[2] = 176;
+        temprature[3] = 'C';
+        temprature[4] = ' ';
+        temprature[5] = '\0';
 
         for(i=0;i<=5;i++){
 
-          UARTwritechar(temp[i]);
+          UARTwritechar(temprature[i]);
         }
         _delay_ms(20);
     }
     else if(temp>=710 && temp<=1024){
         OCR1A=0x3cb;//Output PWM 95%
-        temp[0] = '3';
-        temp[1] = '3';
-        temp[2] = 176;
-        temp[3] = 'C';
-        temp[4] = ' ';
+        temprature[0] = '3';
+        temprature[1] = '3';
+        temprature[2] = 176;
+        temprature[3] = 'C';
+        temprature[4] = ' ';
         temp[5] = '\0';
 
         for(i=0;i<=5;i++){
 
-            UARTwritechar(temp[i]);
+            UARTwritechar(temprature[i]);
     }
         _delay_ms(20);
     }
     else{
         OCR1A = 0x3ff; //Output PWM 100%
-        temp[0] = '0';
-        temp[1] = 'F';
-        temp[2] = 'F';
-        temp[3] = ' ';
-        temp[4] = ' ';
-        temp[5] = '\0';
+        temprature[0] = '0';
+        temprature[1] = 'F';
+        temprature[2] = 'F';
+        temprature[3] = ' ';
+        temprature[4] = ' ';
+        temprature[5] = '\0';
 
         for(i=0;i<=5;i++){
 
-            UARTwritechar(temp[i]);
+            UARTwritechar(temprature[i]);
         }
         _delay_ms(20);
     }
